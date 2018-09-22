767c9660bfe73e6545b87df97babd8a8820647fb
rizalarifnugraha <43487664+rizalarifnugraha@users.noreply.github.com>
2018-09-22 19:26:21 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117048/latihan1/biodata/PBO210117048Latihan1Biodata.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117048.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * Nama  : Rizal Arif Nugraha
+ * Kelas : PBO2 
+ * NIM   : 10117048
+ * Deskripsi Program : Program ini berisi program untuk menampilkan data biodata
+ * author.
+ */
+public class PBO210117048Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" +"---------------------");
+        System.out.println("NAMA\t\t: Rizal Arif Nugraha");
+        System.out.println("ALAMAT\t\t: Jalan Dipatiukur no 96c");
+        System.out.println("TEMPAT LAHIR\t: Indramayu");
+        System.out.println("TANGGAL LAHIR\t: 22 Januari 1999");
+        System.out.println("----------------------");
+        System.out.println("HOBI\t\t: Game dan Nonton");
+        System.out.println("MAKES\t\t: Telor");
+        System.out.println("MIKES\t\t: Milk");
+    }
+    
+}
